30c6ff7ae0a15e0dc9c07bd4343f940d2d1552a4
FatalTsai <Cheney.Tsai@jet-opto.com.tw>
2020-01-13 18:29:27 +0800

    add simple map routing

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,6 +3,7 @@
 //https://developers.google.com/maps/documentation/javascript/tutorial
 import { Component, OnInit, ViewChild, ElementRef, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { Direction } from '@angular/cdk/bidi';
 
 @Component({
   selector: 'app-map',
@@ -31,26 +32,54 @@ export class MapComponent implements OnInit {
     }, 2000)
 
 
+
   }
 
   showMap() {
     
+    const directionsService = new google.maps.DirectionsService();
+    const directionsDisplay = new google.maps.DirectionsRenderer();
+
     console.log(this.mapRef.nativeElement);
     const location = { lat: 25.0723177, lng: 121.574737 }; //it's in Neihu  >///<
     const bikestore ={lat: 25.0723177, lng: 121.574737 }
+    const building101 = { lat: 25.034010, lng: 121.562428 }
     var options = {
       center: location,
-      zoom: 13
+      zoom: 15
     }
 
+    //ref : https://www.oxxostudio.tw/articles/201810/google-maps-19-directions.html
+    //init map
     const map = new google.maps.Map(this.mapRef.nativeElement, options);
     this.addMarket(map, location);
+
+    directionsDisplay.setMap(map);
+
+//routing setting 
+var request : Object = {
+      origin: { lat: 25.034010, lng: 121.562428 },
+      destination: { lat: 25.037906, lng: 121.549781 },
+      travelMode: 'DRIVING'
+  };
+
+
+  directionsService.route(request, function (result, status) {
+    if (status == 'OK') {
+        // reply the detail of each node in routing 
+        console.log(result.routes[0].legs[0].steps);
+        directionsDisplay.setDirections(result);
+    } else {
+        console.log(status);
+    }
+});
+   
   }
+ 
   addMarket(pos, map) {
     return new google.maps.Marker({
       position: pos,
       map: map,
     });
   }
-
 }
