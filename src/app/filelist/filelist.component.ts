5532f8ede8716b51f6f02743d6b22776578d2c62
FatalTsai <cheney1003@gmail.com>
2020-02-07 17:08:21 +0800

    modify folder viewbox into 0 0 20 20

--- src/app/filelist/filelist.component.ts
+++ src/app/filelist/filelist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 export interface Section {
   name: string;
@@ -28,6 +31,16 @@ export class FilelistComponent implements OnInit {
       updated: new Date('1/28/16'),
     }
   ];
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, public router:Router) {
+    iconRegistry.addSvgIcon(
+      'folder_add',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/video-control/folder_add.svg'));
+
+    iconRegistry.addSvgIcon(
+      'zoom-out',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/video-control/folder_minus.svg'));
  
+    }
 
 }
